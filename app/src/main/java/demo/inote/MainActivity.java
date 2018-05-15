5d63ec7cbbb737b1d15cacebfeb8377cc763805e
dharma <dharmaseelan970@gmail.com>
2018-05-15 23:41:48 +0800

    new updates of inotes

--- app/src/main/java/demo/inote/MainActivity.java
+++ app/src/main/java/demo/inote/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test
 
         Button btn = (Button)findViewById(R.id.button_newNotes);
         Button btn2 = (Button)findViewById(R.id.button_myNotes);
